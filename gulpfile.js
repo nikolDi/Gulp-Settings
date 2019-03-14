1b3d9ab94449923338fda6ad28b93b0d0749da41
nikolDi <37365459+nikolDi@users.noreply.github.com>
2019-03-14 23:28:17 +0200

    Add task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
-var gulp = require('gulp');
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/**/*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true}))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}));
+});
 
 
-task
+
+
