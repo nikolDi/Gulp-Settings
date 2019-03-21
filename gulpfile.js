c424274a30443e198dad6bce00e512a53c6c987c
nikolDi <37365459+nikolDi@users.noreply.github.com>
2019-03-21 23:38:30 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
+var gulp    	= require('gulp');
+var sass    	= require('gulp-sass');
+var	browserSync = require('browser-sync');
+var	concat      = require('gulp-concat');
+var	uglify      = require('gulp-uglifyjs');
+var	cssnano     = require('gulp-cssnano');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
 	return gulp.src('app/sass/**/*.scss')
@@ -7,4 +12,45 @@ gulp.task('sass', function() {
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true}))
 		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('css-libs', ['sass'], function() {
+	return gulp.src('app/css/libs.css')
+	.pipe(cssnano())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		//proxy: "название",
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+	gulp.watch('app/sass/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch('app/js/**/*.php', browserSync.reload);
+});
+
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+	var buildCss = gulp.src([
+		'app/css/main.css',
+		'app/css/libs.min.css',
+	])
+		.pipe(gulp.dest('dist/css'));
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*')
+		.pipe(gulp.dest('dist/js'));
+
+	var buildHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
 });
